5c939e145ca54b11763f36a10b8055ef36556e6d
IremRyuk <IremRyuk>
2025-04-22 19:20:53 +0400

    change comments

--- src/Redux/Reducer/FlagRed.jsx
+++ src/Redux/Reducer/FlagRed.jsx
@@ -1,4 +1,6 @@
+// Get Language Information From LocalStorage
 const names = localStorage.getItem('names') || 'namesGeo'
+// Reducer
 export const FlagRed = (state=names,action) => {
     switch (action.type) {
         case 'changeFlag': return state = action.changeFlag
